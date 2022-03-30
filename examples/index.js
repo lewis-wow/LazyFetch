5f65704cb0bcd95c2c0d2ac553e32f206a4dea49
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2022-03-30 08:57:29 +0200

    Create index.js

---
+++ examples/index.js
@@ -0,0 +1,13 @@
+const lf = new LazyFetch();
+
+lf.add("./myFile");
+
+lf.listenScroll(window, 100, ({ disconnect }) => {
+    lf.fetch(5).forEach(async (val) => {
+        val.then(res => res.text()).then(console.log)
+    });
+
+    if (lf.empty()) {
+        disconnect();
+    }
+});
